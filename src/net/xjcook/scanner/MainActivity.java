1e728bf77de4ddc62e8c3f5983aa63b5f123fd67
XjCook <xjcook@gmail.com>
2014-03-06 16:35:11 +0100

    Delete menu

--- src/net/xjcook/scanner/MainActivity.java
+++ src/net/xjcook/scanner/MainActivity.java
@@ -3,7 +3,6 @@ package net.xjcook.scanner;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,13 +15,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 	
 	@Override
